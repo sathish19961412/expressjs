76788e5f2ee79eda7ae856c932efb10fd33b2d3d
sathish19961412 <sathishfreelanc5@gmail.com>
2023-02-28 17:40:58 +0530

    add shop routes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app =express();
 const bodyparser=require('body-parser');
-
 const adminRoutes = require('./routes/admin');
-
+const adminShops =require('./routes/shop');
 app.use(adminRoutes);
+app.use(adminShops);
 app.use(bodyparser.urlencoded())
 app.listen(8080);
