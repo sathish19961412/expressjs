2a5d5ed28ed5cbb9b574f1c9b8f30bfc3d695b63
sathish19961412 <sathishfreelanc5@gmail.com>
2023-02-28 12:42:15 +0530

    Working for url,routes . use for request method in get,post in req and res parameters . Also work to the exports modules in router file to main file

--- app.js
+++ app.js
@@ -2,15 +2,8 @@ const express = require('express');
 const app =express();
 const bodyparser=require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+
+app.use(adminRoutes);
 app.use(bodyparser.urlencoded())
-
-app.use('/add-product',(req,res,next)=>{
-    res.send("<h2>Add Product</h2><form action='/store-product' method='post'><input type='text' name='title'><input type='submit' value='submit'></form>");
-})
-app.use('/store-product',(req,res,next)=>{
-    console.log("form-data",req.body);
-    res.send("<h2>Product Submitted !</h2>");
-})
-
-
 app.listen(8080);
