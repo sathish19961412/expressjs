ae746a377185f5d9e4b10c20ed78c8f1f78a875a
sathish19961412 <sathishfreelanc5@gmail.com>
2023-03-11 09:36:12 +0530

    completed the form redirect to the routes in url

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const router =express.Router();
 
 router.use(bodyparser.urlencoded())
 router.get('/add-product',(req,res,next)=>{
-    res.send("<h2>Add Product</h2><form action='http://localhost:8080/admin/store-product' method='post'><input type='text' name='title'><input type='submit' value='submit'></form>");
+    res.send("<h2>Add Product</h2><form action='http://localhost:8080/admin/add-product' method='post'><input type='text' name='title'><input type='submit' value='submit'></form>");
 })
 router.post('/add-product',(req,res,next)=>{
     console.log("form-data",req.body);
